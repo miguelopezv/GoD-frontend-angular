c7d1632b153663e57c355da5231795825581f96b
Miguel López <miguelopezv@gmail.com>
2019-01-26 21:11:09 -0500

    Add method to create a new player

--- src/app/services/players.service.ts
+++ src/app/services/players.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { BACKEND_URL } from '../shared';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 import { Player } from '../interfaces';
 
 @Injectable({
@@ -11,14 +11,32 @@ import { Player } from '../interfaces';
 export class PlayersService {
   constructor(private _http: HttpClient) {}
 
-  getPlayer(id: number): Observable<Player[]> {
+  /**
+   * Retrieves an existing player from DB
+   * @param player the player to retrieve
+   */
+  getPlayer(player: Player): Observable<Player> {
     let params = new HttpParams();
-    ids.forEach((id, i) => params = params.append('id', id.toString()));
+    params = params.append('firstName', player.firstName);
+    params = params.append('lastName', player.lastName);
 
-    return this._http.get<Player[]>(`${BACKEND_URL}/player`, { params: params })
-    .pipe(map((data: Player[]) => {
-      data.forEach((element: Player) => element.fullName = `${element.firstName} ${element.lastName}`);
+    return this._http.get<Player>(`${BACKEND_URL}/player`, { params: params })
+    .pipe(tap((data: Player) => {
+      data.fullName = `${data.firstName} ${data.lastName}`;
+      return data;
+    }));
+  }
+
+  /**
+   * Persist an user into DB
+   * @param user The user to persist
+   */
+  createPlayer(user: Player): Observable<Player> {
+    return this._http.post<Player>(`${BACKEND_URL}/player`, {...user})
+    .pipe(tap((data: Player) => {
+      data.fullName = `${data.firstName} ${data.lastName}`;
       return data;
     }));
   }
 }
+
