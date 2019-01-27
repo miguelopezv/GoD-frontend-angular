c5349d46cf90b5240007e23cb152a9fb637df41e
Miguel López <miguelopezv@gmail.com>
2019-01-27 05:44:40 -0500

    Add action to update player

--- src/app/actions/player.actions.ts
+++ src/app/actions/player.actions.ts
@@ -4,6 +4,7 @@ import { Player } from '../interfaces/player.interface';
 
 export const ADD_PLAYER = '[PLAYER] Add';
 export const RESET_PLAYERS = '[PLAYER] Reset';
+export const UPDATE_PLAYER = '[PLAYER] Update';
 
 export class AddPlayer implements Action {
   readonly type = ADD_PLAYER;
@@ -17,4 +18,10 @@ export class ResetPlayers implements Action {
   constructor() { }
 }
 
-export type Actions = AddPlayer | ResetPlayers;
+export class UpdatePlayer implements Action {
+  readonly type = UPDATE_PLAYER;
+
+  constructor(public payload: {id: number, changes: Partial<Player>}) {}
+}
+
+export type Actions = AddPlayer | ResetPlayers | UpdatePlayer;
