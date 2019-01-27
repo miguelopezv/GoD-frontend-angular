a6896a277708d64a83151e6f02b86580ee3754bf
Miguel López <miguelopezv@gmail.com>
2019-01-27 05:45:48 -0500

    Update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,39 @@
+import { GameModule } from './game/game.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { GameComponent } from './game/game.component';
 import { StatsComponent } from './stats/stats.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { LoginModule } from './login/login.module';
 
+import { StoreModule, MetaReducer, State, ActionReducerMap } from '@ngrx/store';
+import { storeFreeze } from 'ngrx-store-freeze';
+import { environment } from '../environments/environment';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { HeaderComponent } from './header/header.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import * as fromPlayer from './reducers/player.reducer';
+
+export const metaReducers: MetaReducer<State<any>>[] = !environment.production ? [storeFreeze] : [];
+export const reducers: ActionReducerMap<any> = {
+  player: fromPlayer.playerReducer
+};
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    GameComponent,
-    StatsComponent
-  ],
+  declarations: [AppComponent, StatsComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    LoginModule,
+    GameModule,
+    ReactiveFormsModule,
     HttpClientModule,
-    ReactiveFormsModule
+    StoreModule.forRoot( reducers, { metaReducers }),
+    StoreDevtoolsModule.instrument({maxAge: 10})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
