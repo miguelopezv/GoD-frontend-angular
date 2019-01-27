d0d1e5a47a41a661ceb83f69f2181e8c931c7e17
Miguel López <miguelopezv@gmail.com>
2019-01-27 05:43:29 -0500

    Update reducer with entities

--- src/app/reducers/player.reducer.ts
+++ src/app/reducers/player.reducer.ts
@@ -2,13 +2,21 @@ import { Action } from '@ngrx/store';
 import * as Actions from '../actions/index';
 import { Player } from '../interfaces';
 
-const initialState: Player[] = [];
+import { createEntityAdapter, EntityState, EntityAdapter } from '@ngrx/entity';
 
-export function playerReducer(state: Player[] = initialState, action: Actions.Actions) {
+export interface State extends EntityState<Player> { }
+
+export const playerAdapter: EntityAdapter<Player> = createEntityAdapter<Player>();
+
+export const initialState: State = playerAdapter.getInitialState();
+
+export function playerReducer(state: State = initialState, action: Actions.Actions) {
 
   switch (action.type) {
     case Actions.ADD_PLAYER:
-      return [...state, {...action.payload}];
+      return playerAdapter.addOne(action.payload, state);
+    case Actions.UPDATE_PLAYER:
+      return  playerAdapter.updateOne(action.payload, state);
     case Actions.RESET_PLAYERS:
       return initialState;
     default:
