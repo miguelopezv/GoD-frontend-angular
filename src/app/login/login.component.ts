2f22b8d37ba09c7c0d85eb3735541e539513d2c0
Miguel López <miguelopezv@gmail.com>
2019-01-27 00:57:09 -0500

    Update component with ngrx

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,37 +3,41 @@ import { PlayersService } from './../services/players.service';
 import { Player } from '../interfaces';
 import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { State } from '../interfaces/state.interface';
+import * as PlayerActions from '../actions';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.sass']
 })
+
 export class LoginComponent implements OnInit {
-  // player: Player;
-  players: Player[] = [];
-  playersId: number[] = [];
+  players: Observable<Player[]>;
+  playerNumber: number = 1;
   loginForm: FormGroup;
   existingUser: AbstractControl;
 
   constructor(
     private _playersService: PlayersService,
-    private _router: Router
-    ) {}
+    private _router: Router,
+    private store: Store<State>
+  ) {}
 
   ngOnInit() {
+    this.players = this.store.select('playerFeature');
     this.loginForm = this._createFormGroup();
     this.existingUser = this.loginForm.controls['createdUser'];
   }
 
   /**
-   * Submit the data from the form
+   * Submit the data from the form and cleans it
    */
   onSubmit() {
     this._getPlayer(this.loginForm.value);
-
     this.loginForm.reset();
-    if (this.playersId.length === 2) { this._router.navigateByUrl('/game'); }
   }
 
   /**
@@ -43,17 +47,30 @@ export class LoginComponent implements OnInit {
   private _getPlayer(formData: Player) {
     if (this.existingUser.value) {
       this._playersService.getPlayer(formData)
-      .subscribe((player: Player) => {
-        this.players.push(player);
-        this.playersId.push(player.id);
-      });
+      .subscribe((player: Player) => this._dispatch(player));
     } else {
       this._playersService.createPlayer(formData)
-      .subscribe((player: Player) => {
-        this.players.push(player);
-        this.playersId.push(player.id);
-      });
+      .subscribe((player: Player) => this._dispatch(player));
     }
+  }
+
+  /**
+   * Dispatch the action to update the State
+   * @param player the player to be saved on State
+   */
+  private _dispatch(player: Player) {
+    this.store.dispatch(new PlayerActions.AddPlayer(player));
+    this._checkLength();
+  }
+
+  /**
+   * Checks the length of State to navigate to next page
+   */
+  private _checkLength() {
+    this.players.subscribe(data => {
+      this. playerNumber = data.length + 1;
+      if (this.playerNumber > 2) { this._router.navigateByUrl('/game'); }
+    });
   }
 
   /**
