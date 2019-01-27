da65bbfbece384165cfc7c338b246ada36b0c5dd
Miguel López <miguelopezv@gmail.com>
2019-01-26 19:14:15 -0500

    Add routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { GameComponent } from './game/game.component';
+import { StatsComponent } from './stats/stats.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent},
+  { path: 'game', component: GameComponent},
+  { path: 'stats', component: StatsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
