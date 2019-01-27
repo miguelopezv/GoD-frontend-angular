561f450be6ae93ec75cfd184986e4a5943e1efe4
Miguel López <miguelopezv@gmail.com>
2019-01-26 19:15:01 -0500

    add services

--- src/app/services/players.service.ts
+++ src/app/services/players.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { BACKEND_URL } from '../shared';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Player } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayersService {
+  constructor(private _http: HttpClient) {}
 
-  constructor() { }
+  getPlayer(id: number): Observable<Player[]> {
+    let params = new HttpParams();
+    ids.forEach((id, i) => params = params.append('id', id.toString()));
+
+    return this._http.get<Player[]>(`${BACKEND_URL}/player`, { params: params })
+    .pipe(map((data: Player[]) => {
+      data.forEach((element: Player) => element.fullName = `${element.firstName} ${element.lastName}`);
+      return data;
+    }));
+  }
 }
--- src/app/services/stats.service.ts
+++ src/app/services/stats.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { BACKEND_URL } from '../shared';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Streak } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatsService {
+  constructor(private _http: HttpClient) { }
 
-  constructor() { }
+  getStreak(ids: number[]): Observable<Streak> {
+    let params = new HttpParams();
+    ids.forEach((id, i) => params = params.append(`id${i + 1}`, id.toString()));
+
+    return this._http.get<any>(`${BACKEND_URL}/match`, { params: params })
+      .pipe(tap(data => data));
+  }
 }
