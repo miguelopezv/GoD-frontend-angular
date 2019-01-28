0d0056b3b3a0cc46e37216798470b4f5c2f25716
Miguel López <miguelopezv@gmail.com>
2019-01-28 00:49:46 -0500

    Code refactor, styles and update on template

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Player } from '../interfaces';
 import { Store, State } from '@ngrx/store';
+import { faCrown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -9,18 +9,30 @@ import { Store, State } from '@ngrx/store';
   styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent implements OnInit {
-
   players: Player[];
+  faCrown = faCrown;
+  globalStreak: any[] = [];
 
-  constructor(
-    private store: Store<State<any>>
-  ) { }
+  constructor(private store: Store<State<any>>) {}
 
   ngOnInit() {
     const playersObservable = this.store.select('playerFeature');
     playersObservable.subscribe(players => {
       this.players = Object.values(players.entities);
     });
+
+    const gameObservable = this.store.select('gameFeature');
+    gameObservable.subscribe(streak => {
+      this.globalStreak = Object.values(streak.entities);
+    });
   }
 
+  /**
+   * Converts partials integer into array to iterate crowns
+   * @param n the number of partials wins per user
+   */
+  toArray(n: number): number[] {
+    if (n === null) { n = 0; }
+    return [...Array.from(Array(n).keys())];
+  }
 }
