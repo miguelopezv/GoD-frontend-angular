49b2329949fd12127f78a32692a27cc007312051
Miguel López <miguelopezv@gmail.com>
2019-01-28 00:50:11 -0500

    use on reducer for game module

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -2,13 +2,17 @@ import { GameComponent } from './game.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { gameReducer } from '../reducers/game.reducer';
+import { FooterComponent } from '../footer/footer.component';
 
 @NgModule({
-  declarations: [GameComponent],
+  declarations: [GameComponent, FooterComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    StoreModule.forFeature('gameFeature', gameReducer)
   ],
   exports: [GameComponent]
 })
-export class GameModule { }
+export class GameModule {}
