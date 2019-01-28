470ba5d4621d945a50a10e32348f4b9f932d4aec
Miguel López <miguelopezv@gmail.com>
2019-01-28 00:35:08 -0500

    remove unnecesary returns

--- src/app/services/players.service.ts
+++ src/app/services/players.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BACKEND_URL } from '../shared';
+import { BACKEND_URL } from '../shared/constants';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
@@ -11,10 +11,6 @@ import { Player } from '../interfaces';
 export class PlayersService {
   constructor(private _http: HttpClient) {}
 
-  /**
-   * Retrieves an existing player from DB
-   * @param player the player to retrieve
-   */
   getPlayer(player: Player): Observable<Player> {
     let params = new HttpParams();
     params = params.append('firstName', player.firstName);
@@ -24,19 +20,14 @@ export class PlayersService {
     .pipe(tap((data: Player) => {
       data.fullName = `${data.firstName} ${data.lastName}`;
       data.partials = 0;
-      return data;
     }));
   }
 
-  /**
-   * Persist an user into DB
-   * @param user The user to persist
-   */
   createPlayer(user: Player): Observable<Player> {
     return this._http.post<Player>(`${BACKEND_URL}/player`, {...user})
     .pipe(tap((data: Player) => {
       data.fullName = `${data.firstName} ${data.lastName}`;
-      return data;
+      data.partials = 0;
     }));
   }
 }
