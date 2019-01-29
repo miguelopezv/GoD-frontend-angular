e45cce25ff28f9e645f2671721a385917cef87e7
Miguel López <miguelopezv@gmail.com>
2019-01-28 19:15:44 -0500

    import sharedModule

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -4,13 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { gameReducer } from '../reducers/game.reducer';
-import { FooterComponent } from '../footer/footer.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [GameComponent, FooterComponent],
+  declarations: [GameComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    SharedModule,
     StoreModule.forFeature('gameFeature', gameReducer)
   ],
   exports: [GameComponent]
