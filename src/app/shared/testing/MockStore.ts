1ceff31a0dd9e1160cdb31082bde01e52d9a0670
Miguel López <miguelopezv@gmail.com>
2019-01-29 12:50:17 -0500

    create utils to test components using ngrx

---
+++ src/app/shared/testing/MockStore.ts
@@ -0,0 +1,28 @@
+import { StateObservable, ActionsSubject, ReducerManager, Store } from '@ngrx/store';
+import { BehaviorSubject } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MockStore<T> extends Store<T> {
+  private stateSubject = new BehaviorSubject<T>({} as T);
+
+  constructor(
+    state$: StateObservable,
+    actionsObserver: ActionsSubject,
+    reducerManager: ReducerManager
+  ) {
+    super(state$, actionsObserver, reducerManager);
+    this.source = this.stateSubject.asObservable();
+  }
+
+  setState(nextState: T) {
+    this.stateSubject.next(nextState);
+  }
+}
+
+export function provideMockStore() {
+  return {
+    provide: Store,
+    useClass: MockStore
+  };
+}
