2293dd5c9b036231efb74916a1fa18501266b192
Miguel López <miguelopezv@gmail.com>
2019-01-29 12:16:54 -0500

    login form tests

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,12 +1,27 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from "@angular/forms";
+import { of, Observable } from 'rxjs';
+import { async, ComponentFixture, TestBed, tick } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
 import { PlayersService } from '../services/players.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { StoreModule } from '@ngrx/store';
+import { By } from '@angular/platform-browser';
+import { Player } from '../interfaces';
 
+const result: Player = {
+  id: 1,
+  firstName: 'Miguel',
+  lastName: 'Lopez',
+  wonMatches: [],
+  losedMatches: [],
+  partials: 0
+};
 
-class MockPlayersService {}
+class MockPlayersService {
+  createPlayer(): Observable<Player> {
+    return of(result);
+  }
+}
 
 let mockService: MockPlayersService;
 describe('LoginComponent', () => {
@@ -39,5 +54,24 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be a valid form', () => {
+    const el = fixture.debugElement.query(By.css('button')).nativeElement;
+    component.loginForm.controls['firstName'].setValue('Miguel');
+    component.loginForm.controls['lastName'].setValue('Lopez');
+    component.loginForm.controls['createdUser'].setValue(true);
+
+    expect(component.loginForm.valid).toBeTruthy();
+  });
+
+  it('should call submit method', () => {
+    fixture.detectChanges();
+    const el = fixture.nativeElement.querySelector('button');
+    el.disabled = false;
+    spyOn(component, 'onSubmit');
+    el.click();
+
+    expect(component.onSubmit).toHaveBeenCalledTimes(1);
   });
 });
